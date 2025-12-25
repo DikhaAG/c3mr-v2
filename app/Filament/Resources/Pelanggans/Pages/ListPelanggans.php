5825deadfda161e1938e0cb89fa6d9756c7cb809
DikhaAG <dikhaagsfrdy@gmail.com>
2025-12-25 14:48:06 +0700

    feat(pelanggan/data call): nambahin label

--- app/Filament/Resources/Pelanggans/Pages/ListPelanggans.php
+++ app/Filament/Resources/Pelanggans/Pages/ListPelanggans.php
@@ -14,7 +14,7 @@ class ListPelanggans extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()->label('New Data Call'),
         ];
     }
 }
